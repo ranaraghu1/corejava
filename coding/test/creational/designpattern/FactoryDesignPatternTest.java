45f7538b0a30d4bc83490a55bb4e21b33b94bb7b
raghu <rn@gmail.com>
2025-02-20 16:12:14 +0530

    added comments

--- coding/test/creational/designpattern/FactoryDesignPatternTest.java
+++ coding/test/creational/designpattern/FactoryDesignPatternTest.java
@@ -1,5 +1,6 @@
 package test.creational.designpattern;
-
+//centralized the object creation.
+//separate object creation from client code
 interface Employee{
 	public String getEmployeePriviledge();
 	
