c262e7f90e43ce5b7a3cde2767988f20accade18
raghunath <ranaraghunath@gmail.com>
2025-03-31 06:31:24 +0530

    Add cert folder

---
+++ coding/test/java/cert/MainTest.java
@@ -0,0 +1,24 @@
+package test.java.cert;
+
+/**
+ * This is Java doc comments. It is always on top of class.
+ * @author ranar
+ */
+public class MainTest {
+	
+	/*
+	 * multiline comments.
+	 * Run as Run configuration and pass arguments by providing space, ex- 1 2
+	 * This will print 1 and 2
+	 * If pass single value without space, ex- 12,first args[0] print 12 and args[1] throws ArrayIndexOutOfBoundsException
+	 */
+
+	public static void main(String[] args) {
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+
+	}
+
+	
+
+}
