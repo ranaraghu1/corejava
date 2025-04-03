9639ed28c6789deed66cc4d53ebd0f1bbe38a37e
raghunath <ranaraghunath@gmail.com>
2025-04-03 23:53:55 +0530

    Adding more code

--- coding/test/java/cert/ArrayOperationTest.java
+++ coding/test/java/cert/ArrayOperationTest.java
@@ -3,7 +3,33 @@ package test.java.cert;
 public class ArrayOperationTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		/*
+		 *  array operation
+		 */
+		
+		int[] arr=new int[5];
+		//arr[5]=10; //java.lang.ArrayIndexOutOfBoundsException
+		//arr[-2]=45; //java.lang.ArrayIndexOutOfBoundsException
+		
+		
+		int j=3;
+		double[] array=new double[3];
+		for(int i=0;i>j;i++) {
+			array[0]=i;
+		}
+		
+		for(double val :array) {
+			System.out.println(val);//0.0  0.0  0.0
+			
+		}
+		
+		int element=4;
+		int[] annoArr=new int[] {element};
+		for(int arrElement:annoArr) {
+			System.out.println(arrElement);
+		}
+		
 
 	}
 
