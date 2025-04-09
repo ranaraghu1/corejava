8df6d6528bdd6cd466c2bb17f58b35cc0615b898
raghunath <ranaraghunath@gmail.com>
2025-04-09 18:14:42 +0530

    Added some examples

--- coding/test/java/cert/RelationalOperatorTest.java
+++ coding/test/java/cert/RelationalOperatorTest.java
@@ -6,7 +6,9 @@ public class RelationalOperatorTest {
 		System.out.println('a'<'b');//true
 		System.out.println(100>'a');//true
 		System.out.println(10<10.0);//false
-		System.out.println(10.0<10);//;
+		System.out.println(10.0<10);//false
+		//System.out.println(false<true);//Not valid
+		//System.out.println(2<3<4);// Not valid
 
 	}
 
---
+++ coding/test/java/cert/StringTest.java
@@ -0,0 +1,45 @@
+package test.java.cert;
+
+public class StringTest {
+
+	public static void main(String[] args) {
+		System.out.println("---------------");
+		String s1="abc";
+		String s2="abc";
+		System.out.println(s1==s2);
+		System.out.println(s1.equals(s2));
+		System.out.println(s1.hashCode()==s2.hashCode());
+		
+		System.out.println("---------------");
+		String s11=new String("abc");
+		String s22=new String("abc");
+		System.out.println(s11==s22);
+		System.out.println(s11.equals(s22));
+		System.out.println(s11.hashCode()==s22.hashCode());
+		
+		System.out.println("---------------");
+		StringBuffer sb1=new StringBuffer("abc");
+		StringBuffer sb2=new StringBuffer("abc");
+		System.out.println(sb1==sb2);
+		System.out.println(sb1.equals(sb2));
+		System.out.println(sb1.hashCode()==sb2.hashCode());
+		
+		String s3=new String("raghu");
+		StringBuilder sb3=new StringBuilder("abc");
+		//System.out.println(s3==sb3);//compiletime error
+		
+		System.out.println("-----Bitwise operator----------");
+		System.out.println(true & false);
+		System.out.println(true | false);
+		
+		System.out.println(true ^ false);
+		System.out.println(false ^ true);
+		System.out.println(true ^ true);
+		System.out.println(false ^ false);
+		System.out.println(~5);
+		
+
+
+	}
+
+}
