09e57d182abe87b7a8f70c3da7af4d9ab1187d3d
raghunath <ranaraghunath@gmail.com>
2025-04-21 08:36:19 +0530

    Addding comments

---
+++ coding/test/java/cert/LocalVariableTest1.java
@@ -0,0 +1,16 @@
+package test.java.cert;
+
+public class LocalVariableTest1 {
+	
+	public static void m1() {
+		int x=10;
+		System.out.println(x);
+	}
+
+	public static void main(String[] args) {
+		
+		LocalVariableTest1.m1();
+
+	}
+
+}
